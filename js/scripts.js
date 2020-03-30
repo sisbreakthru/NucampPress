4eb7c8c0698e54cf6fbcd13ead3be75ab1a8ed29
Linda Smith <sisbreakthru@comcast.net>
2020-03-30 19:46:16 -0400

    Added carousel to Testimony section of Homepage

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var scroll = new SmoothScroll('a[href="#content1"]', {
+    speed: 1000
+});
+
+$(function() {
+   /* $("#chevronButton").click(function(){
+        $("#chevronButton").children("i").removeClass("fa-chevron-down");
+    }); */
+});
+
