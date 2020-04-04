bdda7791b392cfe587d4625de27c2e23a779bfb5
Linda Smith <sisbreakthru@comcast.net>
2020-04-04 17:17:54 -0400

    Combined footers; changed chevron in navbar to arrow; added js to remove/display on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,42 @@ var scroll = new SmoothScroll('a[href="#content1"]', {
     speed: 1000
 });
 
-$(function() {
-   /* $("#chevronButton").click(function(){
-        $("#chevronButton").children("i").removeClass("fa-chevron-down");
-    }); */
-});
 
+const chevronButton = document.querySelector("#chevronBtn");
+chevronButton.addEventListener("click", removeArrow)
+
+
+function removeArrow() {
+    var lastScroll = 0;
+  
+    window.onscroll = function() {
+        let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
+       
+        if (currentScroll > 0 && lastScroll <= currentScroll){
+            lastScroll = currentScroll;
+            chevronButton.style.display = "none";
+          }else{
+            lastScroll = currentScroll;
+            chevronButton.style.display = "inline-block";
+          }
+      };
+}
+
+function scrollDetect(){
+    var lastScroll = 0;
+  
+    window.onscroll = function() {
+        let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
+  
+        if (currentScroll > 0 && lastScroll <= currentScroll){
+          lastScroll = currentScroll;
+          chevronButton.style.display = "none";
+        }else{
+          lastScroll = currentScroll;
+          chevronButton.style.display = "inline-block";
+        }
+    };
+  }
+  
+  
+  scrollDetect();
